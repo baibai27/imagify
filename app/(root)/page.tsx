5f3538783e6eedc22c4da064e9afa20b6bce074f
nicole <baijingwen1127@gmail.com>
2024-03-05 21:57:58 -0500

    finished

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,11 +1,49 @@
-import { UserButton } from "@clerk/nextjs";
+import { navLinks } from "@/constants";
 import React from "react";
+import Link from "next/link";
+import Image from "next/image";
+import { Collection } from "@/components/shared/Collection";
+import { getAllImages } from "@/lib/actions/image.actions";
 
-const Home = () => {
+const Home = async ({ searchParams }: SearchParamProps) => {
+  const page = Number(searchParams?.page) || 1;
+  const searchQuery = (searchParams?.query as string) || "";
+  const images = await getAllImages({ page, searchQuery });
+
   return (
-    <div>
-      <p className="">Home</p>
-    </div>
+    <>
+      <section className="home">
+        <h1 className="home-heading">Transform your image with Imagify</h1>
+        <ul className="flex-center w-full gap-20">
+          {navLinks.slice(1, 5).map((link) => (
+            <Link
+              key={link.route}
+              href={link.route}
+              className="flex-center flex-col gap-2"
+            >
+              <li className="flex-center rounded-full bg-white p-4 w-fit">
+                <Image
+                  src={link.icon}
+                  alt={link.label}
+                  width={24}
+                  height={24}
+                />
+              </li>
+              <p className="p-14-medium text-center text-white">{link.label}</p>
+            </Link>
+          ))}
+        </ul>
+      </section>
+
+      <section className="sm:mt-12">
+        <Collection
+          hasSearch={true}
+          images={images?.data}
+          totalPages={images?.totalPage}
+          page={page}
+        />
+      </section>
+    </>
   );
 };
 
--- lib/database/models/image.model.ts
+++ lib/database/models/image.model.ts
@@ -4,7 +4,7 @@ interface Image extends Document {
   title: string;
   transformationType: string;
   publicId: string;
-  secureUrl: string; // URLs can be represented as strings
+  secureURL: string; // URLs can be represented as strings
   width?: number;
   height?: number;
   config?: Object;
@@ -25,11 +25,11 @@ const ImageSchema = new Schema({
   title: { type: String, required: true },
   transformationType: { type: String, required: true },
   publicId: { type: String, required: true },
-  secureUrl: { type: URL, required: true },
+  secureURL: { type: String, required: true },
   width: { type: Number },
   height: { type: Number },
   config: { type: Object },
-  transformationUrl: { type: URL },
+  transformationUrl: { type: String },
   aspectRatio: { type: String },
   color: { type: String },
   prompt: { type: String },
